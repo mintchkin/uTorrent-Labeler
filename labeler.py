01446e61d12407b0e0ce60886348a38800288401
mintchkin <mintchkin.dobfner@gmail.com>
2012-07-01 21:46:08 -0400

    Various bugfixes.

--- labeler.py
+++ labeler.py
@@ -13,51 +13,86 @@ def set_label(hsh, label):
 
 
 def label_from_tracker(hsh):
-    trackers = utorrent.torrent_info(hsh)[hsh].trackers
-    label = ['Other']
+    """Returns a list of labels based on the torrent trackers
+    and 'trackers' data in config.py, else returns default_label"""
+    torrent_trackers = utorrent.torrent_info(hsh)[hsh].trackers
+    label = []
 
-    for tracker in trackers:
-        for announce in tracker_labels.keys():
-            if announce in tracker:
-                label = tracker_labels[announce]
+    for torrent_tracker in torrent_trackers:
+        for tracker_name in trackers.keys():
+            if tracker_name in torrent_tracker and trackers[tracker_name] not in label:
+                label += trackers[tracker_name]
+
+    if len(label) == 0:
+        label = default_label
 
     return label
 
 
 def label_from_type(hsh):
+    """Returns a list of labels based on the extensions of
+    files in torrent and 'filetypes' data in config.py, else
+    returns default_label"""
     files = utorrent.file_list(hsh)[hsh]
-    label = ['Other']
+    label = []
 
     for f in files:
-        ext = os.path.splitext(f.name)[1]
+        ext = os.path.splitext(f.name)[1].lower()
         if ext in filetypes:
-            label = filetypes[ext]
+            for item in filetypes[ext]:
+                if item not in label:
+                    label += filetypes[ext]
+
+    if len(label) == 0:
+        label = default_label
 
     return label
 
 
+def safe_label(hsh):
+    """Uses label_from_type and label_from_tracker to only return
+    a label if it is certain, else returns default_label"""
+    type_labels = label_from_type(hsh)
+    tracker_labels = label_from_tracker(hsh)
+
+    if len(tracker_labels) == 1 and tracker_labels != default_label:
+        return tracker_labels
+    elif len(type_labels) == 1 and type_labels != default_label:
+        return type_labels
+    else:
+        return default_label
+
+
 def smart_label(hsh):
+    """A smart label that uses label_from_type and label_from_tracker
+    to make a "best guess" whenever possible, else returns default_label"""
     label = label_from_tracker(hsh)
-    if label == ['Other']:
+    if label == default_label:
         label = label_from_type(hsh)
     return label
 
 
 def smarter_label(hsh):
-    tracker_labels_list = label_from_tracker(hsh)
-    type_labels_list = label_from_type(hsh)
+    """Returns a more accurate "best guess" in slightly more possible cases than
+    smart_label. Probably should always be used over smart_label"""
+    type_labels = label_from_type(hsh)
+    tracker_labels = label_from_tracker(hsh)
+    label = []
 
-    if tracker_labels_list == ['Other']:
-        return [type_labels_list[0]]
-    elif type_labels_list == ['Other']:
-        return [tracker_labels_list[0]]
+    if type_labels == default_label:
+        label = tracker_labels
+    elif tracker_labels == default_label:
+        label = type_labels
     else:
-        for tracker_label in tracker_labels_list:
-            for type_label in type_labels_list:
+        for tracker_label in tracker_labels:
+            for type_label in type_labels:
                 if type_label == tracker_label:
-                    return [type_label]
+                    label.append(type_label)
 
-    return ['No Label']
+        if len(label) == 0:
+            label = default_label
+
+    return label
 
 
 def label_all(method):
@@ -70,7 +105,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Automatically label torrents.")
     parser.add_argument("hash", default="", nargs='?', help="Hash value of torrent to be labelled.")
     parser.add_argument("-a", "--all", action="store_true", help="Label all torrents using default smart_label.")
-    parser.add_argument("-m", "--method", choices=['label_from_tracker', 'label_from_type', 'smart_label', 'smarter_label'], default='smarter_label', help="Method used for labeling.")
+    parser.add_argument("-m", "--method", choices=['label_from_tracker', 'label_from_type', 'safe_label', 'smart_label', 'smarter_label'], default='safe_label', help="Method used for labeling.")
     parser.add_argument("-s", "--status", default="", help="Status of the torrent (to be passed by uTorrent); will only run if status is 'Downloading'.")
 
     args = parser.parse_args()
@@ -78,20 +113,21 @@ if __name__ == '__main__':
 
     methods = {'label_from_tracker': label_from_tracker,
                'label_from_type': label_from_type,
+               'safe_label': safe_label,
                'smart_label': smart_label,
                'smarter_label': smarter_label}
 
     if (args.hash or args.status) and args.all:
-        print("Too many arguments.")
+        print("ERROR: Too many arguments.")
         exit(1)
     elif args.all:
         label_all(methods[args.method])
         exit(0)
     elif args.status and args.status != 'Downloading':
-        print("Status is not Downloading")
+        print("ERROR: Status is not Downloading")
         exit(1)
     elif args.hash:
-        set_label(args.hash, method[args.method](args.hash))
+        set_label(args.hash, methods[args.method](args.hash)[0])
         exit(0)
     else:
         parser.print_help()
